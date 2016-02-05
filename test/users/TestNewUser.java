1757b0edd2a2da6f21ff52f5b71ad875d66264ca
Martijn Boers <m.balsmeer@gmail.com>
2016-02-05 12:17:19 +0100

    Working mvn test

---
+++ test/users/TestNewUser.java
@@ -0,0 +1,15 @@
+package users;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestNewUser {
+
+	@Test
+	public void test() {
+		// Test
+		// fail("Not yet implemented");
+	}
+
+}
