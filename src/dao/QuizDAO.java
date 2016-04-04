f58e3010136796502bd6c58bf18b065d2dfad5fd
Lars <Lars@192.168.0.108>
2016-04-04 21:25:18 +0200

    correct Query and added attribute id in Mentor

--- src/dao/QuizDAO.java
+++ src/dao/QuizDAO.java
@@ -11,20 +11,23 @@ import java.util.logging.Logger;
 import model.quiz.Answer;
 import model.quiz.Question;
 import model.quiz.Quiz;
+import model.user.Mentor;
 
 public class QuizDAO {
 	protected Logger log = Logger.getGlobal();
 	protected Connection con;
 
-	public List<Quiz> getAllQuizesByMentor(String username) {
+	public List<Quiz> getAllQuizesByMentor(Mentor mentor) {
 		List<Quiz> theQuizes = new ArrayList<Quiz>();
 		
 			PreparedStatement statement;
 			try {
 				statement = con.prepareStatement("SELECT Quiz.name, Quiz.description, Question.question, Question.completed, Answer.answer, Answer.correct"
-						+ "FROM Quiz, Question, Answer,User"
-						+ "WHERE User.username = ?;");
-				statement.setString(1, username);
+						+ "FROM Quiz"
+						+ "JOIN Question on Quiz.quiz_id = Question.quiz_id"
+						+ "JOIN Answer on Question.question_id = Answer.question_id"
+						+ "Where mentor_id = ?;");
+				statement.setInt(1, mentor.getId());;
 				ResultSet result = statement.executeQuery();
 				while(result.next()){
 					Quiz quiz = new Quiz(result.getString("name"),result.getString("description"));
