309d375f37af003e518a8a20f3932ec7112a7a08
Nick van der Laan <nickvdlaan@gmail.com>
2016-04-04 12:15:52 +0200

    added category domain model

---
+++ src/model/category/Category.java
@@ -0,0 +1,16 @@
+package model.category;
+
+public class Category {
+	private String name;
+	public Category() {	}
+	public Category(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
