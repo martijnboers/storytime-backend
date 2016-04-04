f75d0de731a11302651cae562b227bf863890018
Martijn Boers <martijn.s.boers@gmail.com>
2016-04-04 15:39:40 +0200

    DAO super example

---
+++ src/dao/DataAccesObject.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+
+package dao;
+
+import java.sql.Connection;
+
+import logging.Logger;
+
+/**
+ * 
+ * @author martijn
+ *
+ *         This class can be used to parent functions that will be benificial to
+ *         all children DAO's. Function like 'Does x exist'.
+ */
+public class DataAccesObject {
+	protected Logger log = Logger.getInstance();
+	protected Connection con;
+
+	public DataAccesObject() throws Exception {
+		con = ConnectorFactory.getConnection();
+
+	}
+}
