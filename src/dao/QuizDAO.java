6da78323cf9cba2a7b9f8dac4b0c5b70ac71e1b9
Lars <Lars@Lars-pc.home>
2016-04-05 13:03:22 +0200

    Added getAllQuizes() and removed unused imoirts.

--- src/dao/QuizDAO.java
+++ src/dao/QuizDAO.java
@@ -1,13 +1,10 @@
 package dao;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
-
 import model.quiz.Answer;
 import model.quiz.Question;
 import model.quiz.Quiz;
@@ -132,6 +129,58 @@ public class QuizDAO extends DataAccesObject {
 					+ "JOIN Category_has_Quiz ON Quiz.quiz_id = Category_has_Quiz.quiz_id"
 					+ "WHERE Category_has_Quiz.Category_id = ?;");
 			statement.setInt(1, id);
+						
+			ResultSet result = statement.executeQuery();
+			while (result.next()) {
+				Quiz quiz = new Quiz(result.getInt("quiz_id"),result.getString("name"), result.getString("description"));
+				if (!theQuizes.contains(quiz)) {
+					Question question = new Question(result.getString("question"), 
+							result.getBoolean("completed"));
+					Answer answer = new Answer(result.getString("answer"), result.getBoolean("correct"));
+					question.getTheAnswers().add(answer);
+					quiz.getTheQuestions().add(question);
+					theQuizes.add(quiz);
+				} else {
+					for (Quiz q : theQuizes) {
+						Question question = new Question(result.getString("question"), 
+								result.getBoolean("completed"));
+						if (q.equals(quiz)) {
+							if (!q.getTheQuestions().contains(question)) {
+								Answer answer = new Answer(result.getString("answer"), 
+										!result.getBoolean("correct"));
+								question.getTheAnswers().add(answer);
+								q.getTheQuestions().add(question);
+							} else {
+								for (Question qu : q.getTheQuestions()) {
+									if (qu.equals(question)) {
+										Answer answer = new Answer(result.getString("answer"),
+												result.getBoolean("correct"));
+										qu.getTheAnswers().add(answer);
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			statement.close();
+		}
+		return theQuizes;
+	}
+	
+	public List<Quiz> getAllQuizes() throws SQLException {
+		List<Quiz> theQuizes = new ArrayList<Quiz>();
+
+		try {
+			statement = con.prepareStatement(
+					"SELECT Quiz.quiz_id,Quiz.name, Quiz.description, Question.question, Question.completed, Answer.answer, Answer.correct"
+					+ "FROM Quiz"
+					+ "JOIN Question ON Quiz.quiz_id = Question.quiz_id"
+					+ "JOIN Answer ON Question.question_id = Answer.question_id"
+					+ "JOIN Category_has_Quiz ON Quiz.quiz_id = Category_has_Quiz.quiz_id");
 						
 			ResultSet result = statement.executeQuery();
 			while (result.next()) {
