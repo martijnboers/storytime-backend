e4cb607980c093b948de03d5288cbec95a63316e
Martijn Boers <martijn.s.boers@gmail.com>
2016-06-15 14:47:29 +0200

    add program-ab

--- test/quiz/TestQuizControllerNoTravis.java
+++ test/quiz/TestQuizControllerNoTravis.java
@@ -18,7 +18,7 @@ import model.user.Child;
 import model.user.Mentor;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class TestQuizController {
+public class TestQuizControllerNoTravis {
 	private Quiz quiz;
 	private Question question;
 	private Answer answer;
